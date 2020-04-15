e53cde9bfbfb4ef7cec426506189468307d5fef0
caoqi95 <caoqi95@gmail.com>
2020-04-15 15:31:01 +0900

    Update 10.Multiple-dispatch.jl

--- Introduction_to_Julia/10.Multiple-dispatch.jl
+++ Introduction_to_Julia/10.Multiple-dispatch.jl
@@ -28,9 +28,11 @@ foo("hello", "hi")
 This is starting to get to the heart of multiple dispatch. When we declared
 
     foo(x::Int, y::Int) = println("My inputs x and y are both integers!")
+
 we didn't overwrite or replace
 
     foo(y::String, y::String)
+
 Instead, we just added an additional method to the generic function called foo.
 
 A generic function is the abstract concept associated with a particular operation.
@@ -46,9 +48,13 @@ We can use the methods to see how many methods there are for foo.
 println(methods(foo))
 
 #=
-So, we now can call foo on integers or strings. When you call foo on a particular set of arguments, Julia will infer the types of the inputs and dispatch the appropriate method. This is multiple dispatch.
+So, we now can call foo on integers or strings. When you call foo on a particular set of arguments, 
+Julia will infer the types of the inputs and dispatch the appropriate method. This is multiple dispatch.
 
-Multiple dispatch makes our code generic and fast. Our code can be generic and flexible because we can write code in terms of abstract operations such as addition and multiplication, rather than in terms of specific implementations. At the same time, our code runs quickly because Julia is able to call efficient methods for the relevant types.
+Multiple dispatch makes our code generic and fast. Our code can be generic and flexible because we can
+write code in terms of abstract operations such as addition and multiplication, rather than in terms of 
+specific implementations. At the same time, our code runs quickly because Julia is able to call efficient 
+methods for the relevant types.
 
 To see which method is being dispatched when we call a generic function, we can use the @which macro:
 =#
@@ -56,7 +62,8 @@ println(@which foo(3, 4))
 
 
 #=
-And we can continue to add other methods to our generic function foo. Let's add one that takes the abstract type Number, which includes subtypes such as Int, Float64, and other objects you would think of as numbers:
+And we can continue to add other methods to our generic function foo. Let's add one that takes the abstract type Number, 
+which includes subtypes such as Int, Float64, and other objects you would think of as numbers:
 =#
 foo(x::Number, y::Number) = println("My inputs x and y are numbers!")
 
